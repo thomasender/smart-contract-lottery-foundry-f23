d7fdfb7b6e45d1221ecb5137cb5ef530f611c3a1
thomasender <freisingen@yahoo.com>
2024-01-03 18:32:32 +0100

    update intervals in helperconfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -39,7 +39,7 @@ contract HelperConfig is Script {
         return
             NetworkConfig({
                 entranceFee: 0.01 ether,
-                interval: 30,
+                interval: 60,
                 vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
                 subscriptionId: 0,
@@ -84,7 +84,7 @@ contract HelperConfig is Script {
         return
             NetworkConfig({
                 entranceFee: 0.01 ether,
-                interval: 30,
+                interval: 3600,
                 vrfCoordinator: 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed,
                 gasLane: 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f,
                 subscriptionId: 0,
