a0f3acf1d4262e1f7de61332a3ea045b197c1a40
thomasender <freisingen@yahoo.com>
2024-01-05 11:43:44 +0100

    loading spinner and footer

---
+++ raffle-client/src/components/footer.tsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+
+import { Github } from '../icons/github';
+import { LinkedIn } from '../icons/linkedin';
+import { Medium } from '../icons/medium';
+import { ETHERSCAN_MUMBAI_POLYGON_TX_BASE_URL, RAFFLE_ADDRESS } from '../constants';
+import { Polygon } from '../icons/polygon';
+
+
+export const FOOTER_HEIGHT = 120;
+
+const FooterContainer = styled.div`
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    gap: 12px;
+    height: ${FOOTER_HEIGHT}px;
+    background-color: ${({theme}) => theme.colors.background};
+`
+
+const SocialIconsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 24px;
+   
+
+    a {
+        color: ${({theme}) => theme.colors.text};
+    }
+`
+
+const ContractLink = styled.a`
+    display: flex;
+    align-items: center;
+    gap: 12px;
+`
+
+export const Footer = () => {
+  return (
+    <FooterContainer>
+        <SocialIconsContainer>
+            <a href='https://github.com/thomasender/fundme-client' target='_blank'>
+                <Github />
+            </a>
+            <a href='https://www.linkedin.com/in/thomasender/' target='_blank'>
+                <LinkedIn />
+            </a>
+            <a href='https://medium.com/@thomasender' target='_blank'>
+                <Medium />
+            </a>
+        </SocialIconsContainer>
+        <ContractLink href={`${ETHERSCAN_MUMBAI_POLYGON_TX_BASE_URL}${RAFFLE_ADDRESS}`} target="_blank">Fund Me Contract on Polyscan<Polygon /></ContractLink>
+    </FooterContainer>
+  )
+}
