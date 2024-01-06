d40f82c8d6f133836e81ac7d00e95171f9e05c5f
thomasender <freisingen@yahoo.com>
2024-01-06 19:14:02 +0100

    update constants with correct NEXT_DRAW_INTERVAL_IN_MS and new Mumbai Address (1800 seconds)

--- raffle-client/src/constants.ts
+++ raffle-client/src/constants.ts
@@ -3,7 +3,8 @@ import ABI from "./raffle-abi.json"
 
 // OWNER: 0x7B3594AA123CFdFE16fb2d9122D86E34E76B8749
 // MUMBAI Address: 0xcef6bFC4A2DD0B289a4F7bE76F236fd2983bC7ef (Interval 60 seconds)
-export const RAFFLE_ADDRESS = "0xcef6bFC4A2DD0B289a4F7bE76F236fd2983bC7ef";
+// MUMBAI Address: 0xd25F426673FbC77C0967e42dF49b3Fa91655cc43 (Interval 1800 seconds)
+export const RAFFLE_ADDRESS = "0xd25F426673FbC77C0967e42dF49b3Fa91655cc43";
 export const CONTRACT_ABI = ABI as ContractABI;
 // Mumbai Testnet TX BASE URL: https://mumbai.polygonscan.com/tx/
 // Mainnet TX BASE URL: https://polygonscan.com/tx/
@@ -15,4 +16,4 @@ export const POLYGON_MUMBAI_CHAIN_ID = 80001;
 export const POLYGON_MAINNET_CHAIN_ID = 137;
 
 export const POLL_INTERVAL_IN_MS = 5000;
-export const NEXT_DRAW_INTERVAL_IN_MS = 60000;
+export const NEXT_DRAW_INTERVAL_IN_MS = 1800000;
